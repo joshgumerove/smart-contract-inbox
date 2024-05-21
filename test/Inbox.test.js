a96f49c71ea884c06d8e4544a321069a3c290b1d
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-21 17:14:41 -0400

    .nvmrc file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,9 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
+
+console.log("what is assert: ", assert);
