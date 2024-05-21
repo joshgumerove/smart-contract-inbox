bf4571610d87411b74fb20e1c533d18a3298649c
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-21 17:54:26 -0400

    create web3 provider

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,8 +2,4 @@ const assert = require("assert");
 const ganache = require("ganache");
 const { Web3 } = require("web3");
 
-// updated ganache and web3 imports added for convenience
-
-// contract test code will go here
-
-console.log("what is assert: ", assert);
+const web3 = new Web3(ganache.provider());
