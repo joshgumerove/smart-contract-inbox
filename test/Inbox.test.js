c29845d0bccb8b1cfa0d6b19711dc1a404adcfea
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-21 22:10:57 -0400

    deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,17 +3,27 @@ const ganache = require("ganache");
 const { Web3 } = require("web3");
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
-  console.log(accounts);
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
+    });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    assert.equal(10, accounts.length);
+    console.log("inbox: ", inbox);
   });
 });
