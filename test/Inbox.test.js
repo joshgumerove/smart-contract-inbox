4b75c2cbf68d709533a2f22d888a93b991d832f5
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-21 21:37:07 -0400

    test setup for Inbox

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,27 +4,16 @@ const { Web3 } = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return "stopped";
-  }
+let accounts;
 
-  drive() {
-    return "vroom";
-  }
-}
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
 
-let car;
-
-beforeEach(() => {
-  car = new Car();
+  console.log(accounts);
 });
 
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.notEqual([].length, accounts.length);
   });
 });
